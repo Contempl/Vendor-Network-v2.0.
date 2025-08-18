2f0949ef87ba1729a68ed1801327e744b0e16b7f
Contempl <the_mega_gamexc76@mail.ru>
2025-08-18 14:31:39 +0300

    Refactored entitest corresponding to the latest db changes

--- Product.Domain/Entity/Business.cs
+++ Product.Domain/Entity/Business.cs
@@ -1,9 +1,21 @@
-﻿namespace Product.Domain.Entity;
+﻿using Product.Domain.Common;
 
-public abstract class Business : IEntity
+namespace Product.Domain.Entity;
+
+public abstract class Business : IEntityId<int>, IAuditable
 {
 	public int Id { get; set; }
 	public string BusinessName { get; set; }
 	public string Address { get; set; }
 	public string Email { get; set; }
+	
+	public List<User> Users { get; set; }
+	
+	public DateTime CreatedAt { get; set; }
+	
+	public int CreatedBy { get; set; }
+	
+	public DateTime? UpdatedAt { get; set; }
+	
+	public int? UpdatedBy { get; set; }
 }
--- Product.Domain/Entity/VendorFacilityService.cs
+++ Product.Domain/Entity/VendorFacilityService.cs
@@ -1,9 +1,19 @@
-﻿namespace Product.Domain.Entity;
+﻿using Product.Domain.Common;
 
-public class VendorFacilityService : IEntity
+namespace Product.Domain.Entity;
+
+public class VendorFacilityService : IEntityId<int>, IAuditable
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } 
     public int VendorFacilityId { get; set; }
     public VendorFacility VendorFacility { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public int CreatedBy { get; set; }
+    
+    public DateTime? UpdatedAt { get; set; }
+    
+    public int? UpdatedBy { get; set; }
 }
