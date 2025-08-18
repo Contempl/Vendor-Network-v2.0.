57551801f4254121bde7ca00febf56ea8d0b9844
Contempl <the_mega_gamexc76@mail.ru>
2025-08-18 14:14:30 +0300

    Embedded Interceptors

---
+++ Product.Domain/Common/IAuditable.cs
@@ -0,0 +1,12 @@
+namespace Product.Domain.Common;
+
+public interface IAuditable
+{
+    public DateTime CreatedAt { get; set; }
+
+    public int CreatedBy { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public int? UpdatedBy { get; set; }
+}
