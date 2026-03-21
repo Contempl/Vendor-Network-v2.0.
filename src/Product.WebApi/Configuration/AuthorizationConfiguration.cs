8a9ae92c4cc7ca419d792daf3ee1780ebfce54b4
contempl <the_mega_gamexc76@mail.ru>
2026-03-21 18:54:05 +0300

    refactored Program.cs. Moved configurations to separate files

---
+++ src/Product.WebApi/Configuration/AuthorizationConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Product.Domain.Enum;
+
+namespace Product.WebApi.Configuration;
+
+public static class AuthorizationConfiguration
+{
+    public static IServiceCollection ConfigureAuthorization(this IServiceCollection services)
+    {
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("Admin", policy =>
+                policy.RequireRole(UserType.Admin.ToString()));
+
+            options.AddPolicy("VendorUser", policy =>
+                policy.RequireRole(UserType.VendorUser.ToString(), UserType.Admin.ToString()));
+
+            options.AddPolicy("OperatorUser", policy =>
+                policy.RequireRole(UserType.OperatorUser.ToString(), UserType.Admin.ToString()));
+
+            options.AddPolicy("All", policy =>
+                policy.RequireRole(UserType.VendorUser.ToString(),
+                    UserType.OperatorUser.ToString(),
+                    UserType.Admin.ToString()));
+        });
+
+        return services;
+    }
+}
---
+++ src/Product.WebApi/Configuration/WebAppConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Product.WebApi.Middleware;
+
+namespace Product.WebApi.Configuration;
+
+public static class WebAppConfiguration
+{
+    public static IApplicationBuilder ConfigureApp(this WebApplication app)
+    {
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseCors(x => x.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader());
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
+}
