d4ee2411dbc7785f225b25c1a97809f737c92db7
contempl <the_mega_gamexc76@mail.ru>
2026-03-23 15:14:47 +0300

    empty line refactoring

--- src/Product.Infrastructure/Configurations/UserConfiguration.cs
+++ src/Product.Infrastructure/Configurations/UserConfiguration.cs
@@ -8,7 +8,6 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
 {
 	public void Configure(EntityTypeBuilder<User> builder)
 	{
-
 		builder.HasKey(u => u.Id);
 
 		builder.Property(u => u.FirstName)
